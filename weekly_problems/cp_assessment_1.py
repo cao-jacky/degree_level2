8e458c17cece51d69d24df33af0e278bc94c7e2b
Jacky Cao <probablyameow+github@gmail.com>
2016-10-14 14:04:36 +0100

    creating first assignment, and uploading pdf of the task

---
+++ weekly_problems/cp_assessment_1.py
@@ -0,0 +1,12 @@
+from __future__ import division
+import numpy
+import matplotlib.pyplot as pyplot
+
+USER    = "Jacky Cao"
+USER_ID = "bbvw84"
+
+def f(x):
+    '''docstring here'''
+
+def g_bdm(x, dx):
+    '''docstring'''
