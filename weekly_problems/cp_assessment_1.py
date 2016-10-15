f73cc202d9d65f967689f625a3c8f27fdbfd63a8
Jacky Cao <probablyameow+github@gmail.com>
2016-10-15 15:53:00 +0100

    more work done

--- weekly_problems/cp_assessment_1.py
+++ weekly_problems/cp_assessment_1.py
@@ -6,20 +6,28 @@ USER    = "Jacky Cao"
 USER_ID = "bbvw84"
 
 def f(x):
-    '''docstring here'''
-
-    print numpy.cos(x)
+    '''Function which returns a value, or an array of values '''
     return numpy.cos(x)
+
+def g(x):
+    '''adasd'''
+    return -(numpy.sin(x))
 
 def g_bdm(x, dx):
     '''docstring'''
-
-    f_deriv = (f(x) - f(x - dx)) / dx
-
-    print f_deriv
+    return (f(x) - f(x - dx)) / dx
 
 #Creating 100 evenly spaced points between -2*pi and 2*pi
 xs = numpy.linspace(-numpy.pi, numpy.pi, 100)
+
+pyplot.figure(figsize=(8,4))
+df_dx_small = pyplot.plot(xs, g_bdm(xs, dx=1e-7) - g(xs), label='dx small')
+df_dx_good = pyplot.plot(xs, g_bdm(xs, dx=1e-10) - g(xs), label='dx good')
+df_dx_large = pyplot.plot(xs, g_bdm(xs, dx=1e-20) - g(xs), label='dx large')
+pyplot.title("askjdns"); pyplot.legend(loc='upper left')
+pyplot.show()
+
+ANSWER1 =
 
 #Testing the code
 if __name__ == '__main__':
