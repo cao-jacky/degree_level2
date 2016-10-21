d83b2afec1b8493496217f30a2feceed99ef555b
Jacky Cao <probablyameow+github@gmail.com>
2016-10-21 12:45:53 +0100

    copied another function

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -6,11 +6,20 @@ USER = "Jacky Cao"
 USER_ID = "bbvw84"
 
 def f(x):
- return x**2 #add proper indents to this, cannot really do on an ipad 
+ return x**2 # add proper indents to this, cannot really do on an ipad 
 
 def g(x):
- return x**3 / 3
+ return x**3 / 3 # indefinite integral of f(x)
 
 def integrate_simpson(x0, x11, n_panels):
  panel_width = (x1-x0) / n_panels
- sigma = 0 #total area
+ sigma = 0 # total area
+
+ for ix in range(n_panels):
+  #find the left edge of this panel 
+  a = x0 + ix * panel_width
+  #
+  area = area + ???
+
+ return area
+
