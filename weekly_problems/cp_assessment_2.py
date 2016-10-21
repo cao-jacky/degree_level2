af6aa6620926974b483d799d63f2245c522a0e31
Jacky Cao <probablyameow+github@gmail.com>
2016-10-21 12:41:35 +0100

    added more stuff

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -2,4 +2,15 @@ from __future__ import division
 import numpy 
 import matplotlib.pyplot as pyplot 
 
-USER = 
+USER = "Jacky Cao"
+USER_ID = "bbvw84"
+
+def f(x):
+ return x**2 #add proper indents to this, cannot really do on an ipad 
+
+def g(x):
+ return x**3 / 3
+
+def integrate_simpson(x0, x11, n_panels):
+ panel_width = (x1-x0) / n_panels
+ sigma = 0 #total area
