14b7905eca9b2b963cc902e0ad569adac0e231f7
Jacky Cao <probablyameow+github@gmail.com>
2016-10-21 12:56:47 +0100

    finished copying everything from the pdf
    whole example copied from pdf

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -16,10 +16,31 @@ def integrate_simpson(x0, x11, n_panels):
  sigma = 0 # total area
 
  for ix in range(n_panels):
-  #find the left edge of this panel 
+  # find the left edge of this panel 
   a = x0 + ix * panel_width
   #
   area = area + ???
 
  return area
 
+# range of panel sizes to be evaluated and bounds to integrate over
+PANEL_COUNTS = [4, 8, 16, 132, 64, 128, 256, 512, 1024]
+x0, x1 = 0, 2
+
+for n in PANEL_COUNTS:
+ s = integrate_simpson(x0, x1, n)
+ error = abs((s-ref)/ref)
+ y_data.append(error)
+
+pyplot.figure(figsize=(6,6))
+
+pyplot.title("Error scaling with Simpson's rule")
+# Plot with visible markers and a line ('-o') and a marker size of 10 
+pyplot.plot(PANEL_COUNTS, y_data, '-o', ms = 10)
+pyplot.loglog()
+
+pyplot.show()
+
+ANSWER1 = ''' '''
+ANSWER2 = ''' '''
+ 
