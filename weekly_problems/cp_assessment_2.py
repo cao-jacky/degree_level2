8b70baa7c5d85c2ec402741e3744c416b56701b6
Jacky Cao <probablyameow+github@gmail.com>
2016-10-23 00:27:34 +0100

    main bulk of code done

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -6,26 +6,33 @@ USER = "Jacky Cao"
 USER_ID = "bbvw84"
 
 def f(x):
-    return x**2 # add proper indents to this, cannot really do on an ipad
+    return (x**2) * (numpy.sin(x))
 
 def g(x):
-    return x**3 / 3 # indefinite integral of f(x)
+    return (2*x*numpy.sin(x)) - (x**2 - 2)*numpy.cos(x) # indefinite integral of f(x)
 
-def integrate_simpson(x0, x11, n_panels):
-    panel_width = (x1-x0) / n_panels
+def integrate_simpson(x0, x1, n_panels):
+    panel_width = (x1 - x0) / n_panels
     sigma = 0 # total area
 
- for ix in range(n_panels):
-    # find the left edge of this panel
-    a = x0 + ix * panel_width
-    #
-    area = area + ???
+    for ix in range(n_panels):
+        # Find the left edge of this panel
+        a = x0 + ix * panel_width
+        b = a + panel_width
+        m = (a + b) / 2 # Calculating midpoint value as required
+        # Applying values to Simpson's Rule
+        area = ((b-a)/6) * (f(a) + (4*f(m)) + f(b))
+        #
+        sigma += area
 
-    return area
+    return sigma
 
 # range of panel sizes to be evaluated and bounds to integrate over
 PANEL_COUNTS = [4, 8, 16, 132, 64, 128, 256, 512, 1024]
 x0, x1 = 0, 2
+
+y_data = []
+ref = g(x1) - g(x0)
 
 for n in PANEL_COUNTS:
     s = integrate_simpson(x0, x1, n)
