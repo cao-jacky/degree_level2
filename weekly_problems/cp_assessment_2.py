df473634f25339cd99c7fb5dff8096d2acc96cd8
Jacky Cao <probablyameow+github@gmail.com>
2016-10-23 21:03:19 +0100

    more work completed

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -2,7 +2,7 @@ from __future__ import division
 import numpy
 import matplotlib.pyplot as pyplot
 
-USER = "Jacky Cao"
+USER    = "Jacky Cao"
 USER_ID = "bbvw84"
 
 def f(x):
@@ -21,14 +21,13 @@ def integrate_simpson(x0, x1, n_panels):
         b = a + panel_width
         m = (a + b) / 2 # Calculating midpoint value as required
         # Applying values to Simpson's Rule
-        area = ((b-a)/6) * (f(a) + (4*f(m)) + f(b))
+        area = (f(a) + (4*f(m)) + f(b))
         #
         sigma += area
+    return (panel_width/6) * sigma
 
-    return sigma
-
-# range of panel sizes to be evaluated and bounds to integrate over
-PANEL_COUNTS = [4, 8, 16, 132, 64, 128, 256, 512, 1024]
+# Range of panel sizes to be evaluated and bounds to integrate over
+PANEL_COUNTS = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
 x0, x1 = 0, 2
 
 y_data = []
@@ -40,13 +39,16 @@ for n in PANEL_COUNTS:
     y_data.append(error)
 
 pyplot.figure(figsize=(6,6))
-
 pyplot.title("Error scaling with Simpson's rule")
 # Plot with visible markers and a line ('-o') and a marker size of 10
 pyplot.plot(PANEL_COUNTS, y_data, '-o', ms = 10)
+pyplot.xlabel("Number of panels used")
+pyplot.ylabel("Relative error in the numerical method")
 pyplot.loglog()
-
 pyplot.show()
 
-ANSWER1 = ''' '''
-ANSWER2 = ''' '''
+ANSWER1 = '''If you increase the number of panels used, then the accuracy
+increases as the total incremental value for area becomes closer and closer to
+the true analytical value.'''
+ANSWER2 = '''If the function x^2 was integreated instead then if you increased
+the panel count '''
