36c210636118672c0d49a7c560bdc5cc4151d3b1
Jacky Cao <probablyameow+github@gmail.com>
2016-10-24 17:16:38 +0100

    completed

--- weekly_problems/cp_assessment_2.py
+++ weekly_problems/cp_assessment_2.py
@@ -8,23 +8,21 @@ USER_ID = "bbvw84"
 def f(x):
     return (x**2) * (numpy.sin(x))
 
-def g(x):
-    return (2*x*numpy.sin(x)) - (x**2 - 2)*numpy.cos(x) # indefinite integral of f(x)
+def g(x): # Indefinite integral of f(x)
+    return (2*x*numpy.sin(x)) - (x**2 - 2)*numpy.cos(x)
 
 def integrate_simpson(x0, x1, n_panels):
     panel_width = (x1 - x0) / n_panels
     sigma = 0 # total area
 
     for ix in range(n_panels):
-        # Find the left edge of this panel
-        a = x0 + ix * panel_width
-        b = a + panel_width
-        m = (a + b) / 2 # Calculating midpoint value as required
+        l_edge = x0 + ix * panel_width # Finding the left edge of this panel
+        m = l_edge + panel_width / 2 # Calculating midpoint value as required
+        r_edge = l_edge + panel_width
         # Applying values to Simpson's Rule
-        area = (f(a) + (4*f(m)) + f(b))
-        #
-        sigma += area
-    return (panel_width/6) * sigma
+        sigma += ((r_edge-l_edge)/6) * (f(l_edge) + (4*f(m)) + f(r_edge))
+
+    return sigma
 
 # Range of panel sizes to be evaluated and bounds to integrate over
 PANEL_COUNTS = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
@@ -36,6 +34,7 @@ ref = g(x1) - g(x0)
 for n in PANEL_COUNTS:
     s = integrate_simpson(x0, x1, n)
     error = abs((s-ref)/ref)
+    print error
     y_data.append(error)
 
 pyplot.figure(figsize=(6,6))
@@ -50,5 +49,7 @@ pyplot.show()
 ANSWER1 = '''If you increase the number of panels used, then the accuracy
 increases as the total incremental value for area becomes closer and closer to
 the true analytical value.'''
-ANSWER2 = '''If the function x^2 was integreated instead then if you increased
-the panel count '''
+ANSWER2 = '''If the function x^2 was integrated the accuracy would decrease as
+the function x^2 is in the Simpson's Rule expansion itself, so the area of
+calculated with the Rule will be the same as if it was analytically calculated
+so there would be no error.'''
