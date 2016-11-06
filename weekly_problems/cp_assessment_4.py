5eda60cb8ee5f73743fe73443a87c37b757fb0e0
Jacky Cao <probablyameow+github@gmail.com>
2016-11-06 12:31:31 +0000

    various stuff done

--- weekly_problems/cp_assessment_4.py
+++ weekly_problems/cp_assessment_4.py
@@ -8,13 +8,13 @@ USER_ID = "bbvw84"
 
 # Define all constants
 r 			= 0.15 # Radius of cannonball in meters
-rho_iron 	= 7874.00 # Density of iron in kg/m**3
+rho_iron 	= 7874.00 # Density of iron in kgm**-3
 g 			= 9.81 # Acceleration due to gravity in ms**-2
 kappa 		= 0.47 # Drag coefficient of a sphere
 rho_air 	= 1.23 # Density of air
 t1 			= 25.00 # End time for our ODE integration in s
 v0 			= 125.00 # Launch speed in ms**-1
-n_panels 	= 400 # The number of panels to use
+n_panels 	= 400 # Number of panels to use
 
 # Calculating mass and area of cannonball of radium r, of iron
 area = numpy.pi * r**2 # Cross sectional area of the cannon ball in m**2
@@ -34,20 +34,19 @@ def f((x, y, vx, vy), time):
 
 def solve_euler(X0, t1, n_panels):
 	dt = t1 / n_panels # Width of a panel
-	# Initialise simulation parameters
- 	X = X0
-	# Allocate somewhere to store (x,y,vx,vy) at all time points
+ 	X = X0 # Initialise simulation parameters
+	# Allocate array to store (x,y,vx,vy) at all time points
 	history = numpy.zeros((n_panels, len(X0)), dtype=numpy.float32)
 	# Integrate each panel
 	for i in range(n_panels):
 		history[i] = X # Record current values
 		t = i * dt
-		# Calculate next time step
+		# Calculate next time step using Euler
 		X = X + f(X, t)*dt
 	return history
 
 pyplot.figure()
-timebase = numpy.arange(0, t1, t1/n_panels)
+timebase = numpy.arange(0, t1, t1/n_panels) # Timebase for odeint function
 
 def trim_trajectory(values):
 	# Process a trajectory to terminate when it goes below y=0
@@ -59,7 +58,7 @@ def trim_trajectory(values):
 
 proj_range = [] # Range corresponding to each angle
 thetas = range(5, 90, 5) # Angles to explore, in degrees
-thetas = numpy.radians(thetas) # Converting to radians
+thetas = numpy.radians(thetas) # Converting those angles into radians
 
 pyplot.subplot(211) # Full width, half height, on top
 
@@ -76,13 +75,15 @@ for theta in thetas:
 	x_final, y_final, vx_final, vy_final = values_scipy[-1]
 	rnge = x_final - x_first
 	proj_range.append(rnge)
-	pyplot.xlabel("Distance (m)"); pyplot.ylabel("Height (m)")
-	# Plot the odeint trajectory - grey line
+
+	pyplot.xlabel("Range (m)"); pyplot.ylabel("Height (m)")
+	# Plot the odeint trajectory
 	pyplot.plot(values_scipy[:,0], values_scipy[:,1], color='grey',
 	label='odeint')
-	# Plot the Euler trajectory - blue dashed line
+	# Plot the Euler trajectory
 	pyplot.plot(values_euler[:,0], values_euler[:,1], color='blue',
 	label='Euler', linestyle='--')
+	#pyplot.legend(loc='upper right')
 	pyplot.title("Range of iron cannonball")
 
 pyplot.subplot(212)
