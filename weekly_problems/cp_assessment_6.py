bf1afa8620d134d3722da3a24b8ee20fcbf27028
Jacky Cao <probablyameow+github@gmail.com>
2016-11-20 12:40:17 +0000

    bulk code done?

--- weekly_problems/cp_assessment_6.py
+++ weekly_problems/cp_assessment_6.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import numpy
 import matplotlib.pyplot as pyplot
+import matplotlib.colors
 
 USER    = "Jacky Cao"
 USER_ID = "bbvw84"
@@ -9,21 +10,26 @@ USER_ID = "bbvw84"
 xl, xu = -0.2, 1.2  # x-bounds
 yl, yu = -0.2, 1.2  # y-bounds
 x0, y0 = 0.2, 1.0   # Initial position
-n_panels = 1000
+r0 = numpy.array((x0,y0))
+n_panels = 10000
 
-## zkjdnasnd
 # Exploring 1000 points in x and y
 N_POINTS = 1000
 dx = (xu-xl)/N_POINTS
 dy = (yu-yl)/N_POINTS
 
-# Generate x and y values
-x_axis = numpy.arange(xl, xu, dy)
 y_axis = numpy.arange(yl, yu, dx)
+x_axis = numpy.arange(xl, xu, dy)
+
+dat = numpy.zeros((len(y_axis), len(x_axis)))
 
 def f((x, y)):
     """ Function at vec(r) """
     return (1-x)**2 + 100*(y-x**2)**2
+
+for iy, y in enumerate(y_axis):
+    for ix, x in enumerate(x_axis):
+        dat[iy, ix] = f((x, y))
 
 def grad((x, y)):
     """ Vector differential of f(vec(r)) """
@@ -32,26 +38,26 @@ def grad((x, y)):
     return numpy.array((df_dx, df_dy))
 
 def gradient_descent((x,y)):
-    r0 = numpy.array((x0,y0))
-    gamma = 0.05                 # Gamma parameter, step size
-    r = numpy.array((x,y))
+    gamma = 0.001              # Gamma parameter, step size
+    r = r0
 
     r_history = numpy.zeros((n_panels, 3), dtype=numpy.float32)
 
     for i in range(n_panels):
-        r = r0
-        r_history[i] = r # Record current values
+        r_history[i,:2] = r # Record current values
 		# Calculate next time step
         r = r - gamma * grad(r) # Euler time step involving f(n)
-	#print r_history
 
-#pyplot.figure()
-#pyplot.show()
+    print r_history
+    return r_history
 
-r_history = numpy.zeros((n_panels, 3), dtype=numpy.float32)
-r_history[0][0] = 1
-r_history[0][1] = 2
-print r_history[0]
+data = gradient_descent(r0)
+
+pyplot.figure()
+im = pyplot.imshow(dat, extent=(xl, xu, yl, yu), origin='lower', cmap=matplotlib.cm.gray, norm=matplotlib.colors.LogNorm(vmin=0.2, vmax=200))
+pyplot.plot(data[:,0], data[:,1], '-o')
+pyplot.colorbar(im, orientation='vertical')
+pyplot.show()
 
 ANSWER1 = """ """
 ANSWER2 = """ """
