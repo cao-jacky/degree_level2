95b59345310c67dcd28c82a5cd0e21009b45fd6e
Jacky Cao <probablyameow+github@gmail.com>
2016-11-25 19:30:02 +0000

    cp7 begins

---
+++ weekly_problems/cp_assessment_7.py
@@ -0,0 +1,24 @@
+from __future__ import division
+import numpy
+import matplotlib.pyplot as pyplot
+import matplotlib.colors
+
+USER    = "Jacky Cao"
+USER_ID = "bbvw84"
+
+# Define initial variables
+x0, y0 = 30, 20     # Initial position of bacteria
+v = 2               # Constant speed in microns per second
+k = 2               # Sensitivity
+tumble_time = 0.1   # Tumble event duration in seconds
+
+def f(x,y):
+    return 2000 - (x**2 + y**2)
+
+def bacteria(x,y):
+    bacteria_history = numpy.zeros()
+    return bacteria_history
+
+pyplot.figure()
+pyplot.plot()
+pyplot.show()
