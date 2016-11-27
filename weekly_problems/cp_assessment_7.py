f3976102a207909129c14015f855d7e1ad311abd
Jacky Cao <probablyameow+github@gmail.com>
2016-11-27 22:55:00 +0000

    version with average

--- weekly_problems/cp_assessment_7.py
+++ weekly_problems/cp_assessment_7.py
@@ -15,7 +15,7 @@ v = 2                       # Constant speed in microns per second
 k = 0.2                     # Sensitivity
 max_steps = 1000            # Max steps, 100 seconds, each step is 0.1s
 dt = 0.1                    # Time step in seconds
-bacterias = 5               # Number of bacteria being launched
+bacterias = 20              # Number of bacteria being launched
 
 # Exploring 1000 points in x and y
 N_POINTS = 1000
@@ -26,61 +26,48 @@ y_axis = numpy.arange(yl, yu, dy) # Generating points between lower and upper y
 x_axis = numpy.arange(xl, xu, dx) # Generating points between lower and upper x
 points = numpy.zeros((len(y_axis), len(x_axis)))        # Generating points
 
-def f((x,y)):
-    """ Energy density function """
+def f((x,y)): # Energy density function
     return 2000 - (x**2 + y**2)
 
 for iy, y in enumerate(y_axis):     # Explore all points and populate array
     for ix, x in enumerate(x_axis):
         points[iy, ix] = f((x, y))
 
-def random_generator(p):
-    """Generates a different random number to use each time called"""
+def random_generator(): # Generate a random number to use each time called
     random_position = numpy.random.uniform(size=(1,1))
-    random_coord = numpy.random.uniform(size=(1,2))
-    return random_position if p == 1 else random_coord
+    return random_position
 
 def bacteria((x,y)):
     # Initial system for each bacterium
-    bacteria_history = numpy.zeros((max_steps, 5), dtype=numpy.float32)
+    bacteria_history = numpy.zeros((max_steps, 6), dtype=numpy.float32)
     r = r0
-    angle = 2 * numpy.pi * random_generator(1)
+    angle = 2 * numpy.pi * random_generator()
     shift = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
     for i in range(max_steps):
         bacteria_history[i][2] = i # Storing time of step
         bacteria_history[i,:2] = r # Storing position of molecule
-
         # Mean square displacement calculations
         posn_0 = bacteria_history[0,:2] # Particle position at time 0
         posn_max = numpy.array((0,0))   # Position of maximum energy
         posn_t = r                      # Particle at time 'i'
-
         msd_origin = numpy.average((posn_t-posn_0)**2)  # MSD from initial posn
         msd_max = numpy.average((posn_t-posn_max)**2)   # MSD from max E posn
-
-        bacteria_history[i][3] = msd_origin
-        bacteria_history[i][4] = msd_max
-
-        # Energy minisation calculations
+        bacteria_history[i][3] = msd_origin # Index the value of msd_origin
+        bacteria_history[i][4] = msd_max    # Index the value of msd_max
+        bacteria_history[i][5] = numpy.average(msd_origin + msd_max)
+        # Energy minimisation calculations
         eNew = f(r.flatten())       # New energy level
         shift.append(eNew)          # Add to the 'shift' list
         shift = shift[-10:]         # Keep only the 10 most recent entries
         de = shift[-1] - shift[0]   # [-1] is newest entry, [0] is the oldest
-
         t_half = 1 + k * (de/dt)
-
         mean_life = t_half / numpy.log(2)
-
-        # Calculating probabilites of tumbling
-        if t_half < 0:
+        if t_half < 0: # Calculating probabilites of tumbling
             p_nt = 1
         else:
             p_nt = 1 - numpy.exp(-dt / mean_life)
-
-        if random_generator(1) < p_nt: # if the randomly generated probability is less than probability of not tumbling, then tumble
-            # Tumbling
-            angle = 2 * numpy.pi * random_generator(1)
+        if random_generator() < p_nt: # Generate new angle or continue on
+            angle = 2 * numpy.pi * random_generator()
         else:
             x_coord = v * dt * numpy.cos(angle) # Random direction's x_coord
             y_coord = v * dt * numpy.sin(angle) # Random direction's y_coord
@@ -89,21 +76,25 @@ def bacteria((x,y)):
     return bacteria_history
 
 pyplot.figure()
+pyplot.title('')
 for i in range(bacterias):
-    bacteria_data = bacteria(r0)
-    pyplot.subplot(221)
-    pyplot.plot(bacteria_data[:,0], bacteria_data[:,1])
+    b_d = bacteria(r0) # Bacteria data
+    pyplot.subplot(221)     # Energy field and bacteria tracks
+    pyplot.plot(b_d[:,0], b_d[:,1])
     pyplot.imshow(points, extent=(xl, xu, yl, yu), origin='lower',
     cmap=matplotlib.cm.gray)
     pyplot.xlabel("x-axis $(\mu m )$"); pyplot.ylabel("y-axis $(\mu m)$")
-
-    pyplot.subplot(222)
-    pyplot.plot((bacteria_data[0][0], bacteria_data[-1][0]), (bacteria_data[0][1], bacteria_data[-1][1]), '-o')
+    pyplot.subplot(222)     # Simplified trajectorys
+    pyplot.plot((b_d[0][0], b_d[-1][0]), (b_d[0][1], b_d[-1][1]), '-o')
     pyplot.xlabel("x-axis $(\mu m )$"); pyplot.ylabel("y-axis $(\mu m)$")
-
-    pyplot.subplot(212)
-    pyplot.plot(bacteria_data[:,2], bacteria_data[:,3], color='red')
-    pyplot.plot(bacteria_data[:,2], bacteria_data[:,4], color='blue')
+    pyplot.subplot(212)     # Mean Square Displacements
+    pyplot.plot(b_d[:,2], b_d[:,3], '-r', alpha=0.1)
+    pyplot.plot(b_d[:,2], b_d[:,4], '-b', alpha=0.1)
     pyplot.xlabel("Time $(s)$"); pyplot.ylabel("MSD $({\mu m}^2)$")
-
+pyplot.plot(b_d[:,2], b_d[:,5], '-b')
+pyplot.plot(0,0, '-r', label='Initial point, $(0,0)$')
+pyplot.plot(0,0, '-b', label='Max energy')
+pyplot.legend(loc='lower right')
 pyplot.show()
+
+ANSWER1 = """  """
