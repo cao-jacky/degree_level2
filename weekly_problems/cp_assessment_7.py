effc08a4732effa1aca33dc0fc4d8868db53bab1
Jacky Cao <probablyameow+github@gmail.com>
2016-11-27 21:22:27 +0000

    finished cp7 maybs

--- weekly_problems/cp_assessment_7.py
+++ weekly_problems/cp_assessment_7.py
@@ -7,14 +7,14 @@ USER    = "Jacky Cao"
 USER_ID = "bbvw84"
 
 # Define initial variables
-xl, xu = -10, 50             # x-bounds
-yl, yu = -10, 50             # y-bounds
+xl, xu = -10, 40             # x-bounds
+yl, yu = -10, 40             # y-bounds
 x0, y0 = 30, 20     # Initial position of bacteria
 v = 2               # Constant speed in microns per second
-k = 0.2               # Sensitivity
+k = 0.2             # Sensitivity
 tumble_time = 0.1   # Tumble event duration in seconds
 total_time = 100    # Total time of function running, in seconds
-max_steps = 1000    # Max steps, 100 seconds
+max_steps = 1000    # Max steps, 100 seconds, each step is 0.1s
 time_step = 0.1     # Time step, in seconds
 
 r0 = numpy.array((x0,y0))
@@ -44,43 +44,42 @@ def random_generator(p):
     return random_position if p == 1 else random_coord
 
 def bacteria((x,y)):
-    bacteria_history = numpy.zeros((max_steps, 2), dtype=numpy.float32) # Initial array to store bacteria information
+    bacteria_history = numpy.zeros((max_steps, 5), dtype=numpy.float32) # Initial array to store bacteria information
     r = r0
-    dt = 1 # Time step in seconds
-    angle = 3.73
+    dt = 0.1 # Time step in seconds
+    angle = 2 * numpy.pi * random_generator(1)
 
-    a = 0
-
-    shift = [a, a, a, a, a, a, a, a, a, a]
-    increasing, decreasing = 0, 0
-    print "begins"
+    shift = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     for i in range(max_steps):
-        bacteria_history[i,:2] = r
-        #print r
+        bacteria_history[i][2] = i # Storing time of step
+        bacteria_history[i,:2] = r # Storing position of molecule
 
-        #p_nt = 0.5
+        # Mean square displacement, calculations
+        ## MSD from origin
+        posn_0 = bacteria_history[0,:2] # Particle position at time 0
+        posn_max = numpy.array((0,0))
+        posn_t = r                # Particle at time 'i'
+
+        msd_origin = numpy.average((posn_t-posn_0)**2) # MSD from origin
+        msd_max = numpy.average((posn_t-posn_max)**2) # MSD from max
+
+        bacteria_history[i][3] = msd_origin
+        bacteria_history[i][4] = msd_max
+
         eNew = f(r.flatten()) # New energy level
         shift.append(eNew) # add to the python list
         shift = shift[-10:] # keep only the 10 most recent entries
         de = shift[-1] - shift[0] # [-1] is the newest entry, [0] is the oldest
-        #print 'de is:', de
-        #print shift
+
         t_half = 1 + k * (de/dt)
-
-        if t_half < 0:
-            t_half = - t_half
-
-        print 'half-life is:', t_half
 
         mean_life = t_half / numpy.log(2)
 
-        if t_half < 0.1:
-            #p_nt = 0
-            b = 0
+        if t_half < 0:
+            p_nt = 1
         else:
-            p_nt = numpy.exp(-dt / mean_life) # Probability of not tumbling, i.e. not changing coordinate, it's happy
-            #print 'probability of not tumbling is:', p_nt
+            p_nt = 1 - numpy.exp(-dt / mean_life) # Probability of not tumbling, i.e. not changing coordinate, it's happy
 
         # p_nt is a varibale, it changes, we need to change it to account for the energy field - make it related to the energy field
 
@@ -96,17 +95,31 @@ def bacteria((x,y)):
 
     return bacteria_history
 
-bacterias = 2     # Number of bacteria being launched
+bacterias = 5    # Number of bacteria being launched
 
 pyplot.figure()
+
 for i in range(bacterias):
     bacteria_data = bacteria(r0)
+    pyplot.subplot(221)
     pyplot.plot(bacteria_data[:,0], bacteria_data[:,1])
+    im = pyplot.imshow(points, extent=(xl, xu, yl, yu), origin='lower',
+    cmap=matplotlib.cm.gray)
+    pyplot.xlabel("x-axis $(\mu m )$"); pyplot.ylabel("y-axis $(\mu m)$")
+
+    pyplot.subplot(222)
+    pyplot.plot((bacteria_data[0][0], bacteria_data[-1][0]), (bacteria_data[0][1], bacteria_data[-1][1]), '-o')
+    pyplot.xlim(-10, 40); pyplot.ylim(-10, 30)
+    pyplot.xlabel("x-axis $(\mu m )$"); pyplot.ylabel("y-axis $(\mu m)$")
+
+    pyplot.subplot(212)
+    pyplot.plot(bacteria_data[:,2], bacteria_data[:,3], color='red')
+    pyplot.plot(bacteria_data[:,2], bacteria_data[:,4], color='blue')
+    pyplot.xlabel("Time $(s)$"); pyplot.ylabel("MSD $({\mu m}^2)$")
 
 
-im = pyplot.imshow(points, extent=(xl, xu, yl, yu), origin='lower',
-cmap=matplotlib.cm.gray)
-pyplot.xlabel("x-axis (microns)"); pyplot.ylabel("y-axis (microns)")
+
+
 pyplot.show()
 
 numpy.savetxt('bacteria_history.txt', bacteria_data, delimiter=',')   # X is an array
