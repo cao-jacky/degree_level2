68986b08279085817373c5b81640bf328d8a0040
Jacky Cao <probablyameow+github@gmail.com>
2016-12-05 01:14:56 +0000

    found example of fractals

--- weekly_problems/cp_assessment_8_testing.py
+++ weekly_problems/cp_assessment_8_testing.py
@@ -1,37 +1,43 @@
-# Newton fractals
-# FB - 201003291
-from Pillow import Image
-imgx = 512
-imgy = 512
-image = Image.new("RGB", (imgx, imgy))
+# Source: http://code.activestate.com/recipes/577166-newton-fractals/
 
-# drawing area
-xa = -1.0
-xb = 1.0
-ya = -1.0
-yb = 1.0
+from PIL import Image
+from cmath import *
 
-maxIt = 20 # max iterations allowed
-h = 1e-6 # step size for numerical derivative
-eps = 1e-3 # max error allowed
+# creates a z**4+1 = 0 fractal using the Newton-Raphson
+# root finding method
+delta       = 0.000001  # convergence criteria
+res         = 800       # image size
+iters       = 30        # number of iterations
 
-# put any complex function here to generate a fractal for it!
-def f(z):
-    return z * z * z - 1.0
+# create an image to draw on, paint it black
+img=Image.new("RGB", (res, res), (0, 0, 0))
 
-# draw the fractal
-for y in range(imgy):
-    zy = y * (yb - ya) / (imgy - 1) + ya
-    for x in range(imgx):
-        zx = x * (xb - xa) / (imgx - 1) + xa
-        z = complex(zx, zy)
-        for i in range(maxIt):
-            # complex numerical derivative
-            dz = (f(z + complex(h, h)) - f(z)) / complex(h, h)
-            z0 = z - f(z) / dz # Newton iteration
-            if abs(z0 - z) < eps: # stop when close enough to any root
+# these are the solutions to the equation z**4+1 = 0 (Euler's formula)
+solutions = [cos((2*n+1)*pi/4)+1j*sin((2*n+1)*pi/4) for n in range(4)]
+colors = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0)]
+
+for re in range(0, res):
+    for im in range(0, res):
+        z = (re+1j*im)/res
+        for i in range(iters):
+            try:
+                z -= (z**4+1)/(4*z**3)
+            except ZeroDivisionError:
+                # possibly divide by zero exception
+                continue
+            if(abs(z**4+1)<delta):
                 break
-            z = z0
-        image.putpixel((x, y), (i % 4 * 64, i % 8 * 32, i % 16 * 16))
 
-image.save("newtonFr.png", "PNG")
+        # color depth is a function of the number of iterations
+        color_depth = int((iters-i)*255.0/iters)
+
+        # find to which solution this guess converged to
+        err = [ abs(z-root) for root in solutions ]
+        distances = zip(err, range(len(colors)))
+
+        # select the color associated with the solution
+        color = [i*color_depth for i in colors[min(distances)[1]]]
+        img.putpixel((re, im), tuple(color))
+
+img.save('fractal_z4s_%03d_%03d_%03d.png' % \
+    (iters, res, abs(log10(delta))), dpi=(150,150))
