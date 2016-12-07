cb90fb4314bbe72b2628b163996150925007fc66
Jacky Cao <probablyameow+github@gmail.com>
2016-12-07 17:52:05 +0000

    oo fancy cp8

--- weekly_problems/cp_assessment_8.py
+++ weekly_problems/cp_assessment_8.py
@@ -13,17 +13,26 @@ def newton_raphson(z):
         z = z - (z**4 - 1) / (4*z**3)
     return z
 
+def newton_raphson_ey(z):
+    for i in range(500):
+        zLast = z
+        z = z - (z**4 - 1) / (4*z**3)
+        #print z, zLast
+        #if abs(numpy.absolute(z)-numpy.absolute(zLast)) < 1e-6:
+            #break
+    return numpy.angle(z)
+
 def newton_raphson2(z):
     for i in range(15):
         zLast = z
         z = z - (z**4 - 1) / (4*z**3)
-        print z, zLast
+        #print z, zLast
         if abs(numpy.absolute(z)-numpy.absolute(zLast)) < 1e-6:
             break
     return i
 
-x_values = numpy.linspace(-10,10,steps)
-y_values = numpy.linspace(-10,10,steps)
+x_values = numpy.linspace(-1.5,1.5,steps)
+y_values = numpy.linspace(-1.5,1.5,steps)
 xx, yy = numpy.meshgrid(x_values, y_values)
 
 # Converts the values returned from Newton Raphson into an image of step x step
@@ -39,7 +48,16 @@ for i in range(npts):
     for j in range(npts):
         z = xs[i]+ xs[j]*1j
         zs[i,j] = newton_raphson2(z)
-print zs
+
+npts2 = 300; xmin2 = -1.5; xmax2 = 1.5
+xs2 = numpy.linspace(xmin2, xmax2, npts2)
+tmp2 = numpy.zeros(npts2, dtype=int)
+zs2 = numpy.outer(tmp2, tmp2)  # A square array
+
+for i in range(npts2):
+    for j in range(npts2):
+        z2 = xs2[i]+ xs2[j]*1j
+        zs2[i,j] = newton_raphson_ey(z2)
 
 #pic2 = numpy.reshape(newton_raphson(numpy.ravel(xx+yy*1j)),[steps,steps])
 
@@ -53,9 +71,12 @@ print zs
 pyplot.figure()
 pyplot.subplot(221)
 im = pyplot.imshow(numpy.angle(pic), origin='lower')
-pyplot.colorbar(im, orientation='vertical')
+#pyplot.colorbar(im, orientation='vertical')
 
 pyplot.subplot(222)
-pyplot.imshow(zs, origin='lower');
+pyplot.imshow(zs, origin='lower')
+
+pyplot.subplot(223)
+pyplot.imshow(zs2, origin='lower', cmap='Accent')
 
 pyplot.show()
