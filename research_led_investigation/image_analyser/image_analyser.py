82f8aa6088a929684300496d8b892692cb12c229
Jacky Cao <probablyameow+github@gmail.com>
2017-02-10 19:38:12 +0000

    image_analyser.py began

---
+++ research_led_investigation/image_analyser/image_analyser.py
@@ -0,0 +1,16 @@
+# Standard libraries being imported
+from __future__ import division
+import numpy
+import matplotlib.pyplot as pyplot
+# Importing Python Image Library
+from PIL import Image
+
+# What functions do I want?
+
+# Function to process image and turn into RGB values
+
+# Function to turn image into 'graph' histogram
+
+# Function to process image and measure the distances between intensity points
+
+im = Image.open("lena.ppm")
