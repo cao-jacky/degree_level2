58cd164f35ff0aa65ccbf18588f1d4fbd1c03cf5
Jacky Cao <probablyameow+github@gmail.com>
2017-03-17 16:41:44 +0000

    more testing of plotting functions

--- matlab/test.m
+++ matlab/test.m
@@ -1,7 +1,18 @@
+%% Testing of addition
 x = 1
 y = 2
 z = x + y
 disp(z)
 
+%% Testing of pi
 a1 = pi
 disp(a1)
+
+x1 = linspace(0,2*pi)
+% y1 = [1,2,3,4,5,6,7,8,9,10]
+
+% plot(x1,y1)
+%% Plotting sine
+plot(sin(x1))
+%% Plotting cosine
+plot(cos(x1))
